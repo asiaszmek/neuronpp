185aac73a604a3a3ce82c25996506341a86ec715
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.edu.pl>
2020-07-15 10:51:56 +0200

    Add Migliore's pyramidal cell

---
+++ neuronpp/cells/migliore2018_cell.py
@@ -0,0 +1,78 @@
+import os
+
+from neuronpp.cells.cell import Cell
+from neuronpp.core.cells.core_hoc_cell import CoreHocCell
+
+path = os.path.dirname(os.path.abspath(__file__))
+f_path = os.path.join(path, "..", "commons/hocmodels/migliore2018")
+
+class CA1_04(Cell, CoreHocCell):
+    def __init__(self, name=None, model_folder=f_path, spine_number=0,
+                 spine_secs_names="apic",
+                 spine_seed: int = None):
+        """
+        :param name:
+            The name of the cell
+        :param model_folder:
+            The folder where the main folder of Combe et al. 2018 model is located
+        :param spine_number:
+            The number of spines added to the model with random_uniform distribution to the sections
+             specified by 'spine_sec' param.
+        :param spine_secs_names:
+            The section or sections where to put spines. It can be:
+              * a string - as a filter name, so you can set "apic" to add spies to all apical
+                dendrites
+
+              * a regex, which need to be prefixed with 'regex:' string before
+                eg. 'regex:(apic)|(basal)'
+              will return all sections wich have a name containing 'apic' or 'basal' string
+
+              * a list of existing sections in the cell
+        :param spine_seed:
+            Seed value for the random_uniform spike distribution. Default is None
+            meaning - there is no seed
+        """
+        Cell.__init__(self, name, model_folder)
+        CoreHocCell.__init__(self, name)
+
+        main_file = os.path.join(model_folder, "load_cell_04_pyr.hoc")
+        self.load_hoc(main_file, hoc_template_name="CA1_PC_cAC_sig5")
+
+        
+        # Add spines with AMPA and NMDA synapses
+
+
+class CA1_08(Cell, CoreHocCell):
+    def __init__(self, name=None, model_folder=f_path, spine_number=0,
+                 spine_secs_names="apic",
+                 spine_seed: int = None):
+        """
+        :param name:
+            The name of the cell
+        :param model_folder:
+            The folder where the main folder of Combe et al. 2018 model is located
+        :param spine_number:
+            The number of spines added to the model with random_uniform distribution to the sections
+             specified by 'spine_sec' param.
+        :param spine_secs_names:
+            The section or sections where to put spines. It can be:
+              * a string - as a filter name, so you can set "apic" to add spies to all apical
+                dendrites
+
+              * a regex, which need to be prefixed with 'regex:' string before
+                eg. 'regex:(apic)|(basal)'
+              will return all sections wich have a name containing 'apic' or 'basal' string
+
+              * a list of existing sections in the cell
+        :param spine_seed:
+            Seed value for the random_uniform spike distribution. Default is None
+            meaning - there is no seed
+        """
+        Cell.__init__(self, name, model_folder)
+        CoreHocCell.__init__(self, name)
+
+        main_file = os.path.join(model_folder, "load_cell_08_pyr.hoc")
+        self.load_hoc(main_file, hoc_template_name="CA1_PC_cAC_sig6")
+
+        
+        # Add spines with AMPA and NMDA synapses
---
+++ neuronpp/examples/migliore2018_iv_curves.py
@@ -0,0 +1,56 @@
+import neuron
+import numpy as np
+import matplotlib.pyplot as plt
+from neuronpp.cells.migliore2018_cell import CA1_04
+
+
+# Create cell
+neuron.h.CVode()
+neuron.h.CVode().active(True)
+
+cell = CA1_04(name="cell1")
+soma = cell.filter_secs("soma")
+
+fig, ax = plt.subplots(1, 1)
+injections = [-.1, 0, .100, .200]
+neuron.h.celsius = 34
+
+for inj in injections:
+    print(inj)
+    ic = neuron.h.IClamp(soma.hoc(0.5))
+    rec_v = neuron.h.Vector().record(soma.hoc(0.5)._ref_v)
+    time = neuron.h.Vector().record(neuron.h._ref_t)
+    ic.delay = 300
+    ic.dur = 1000
+    ic.amp = inj
+    neuron.h.finitialize(-70)
+    neuron.h.fcurrent()
+    neuron.run(1500)
+    ax.plot(time, rec_v, label="%4.3fnA" % inj)
+
+f1 = open("combe_mine_apic.txt", "w")
+f2 = open("combe_mine_trunk.txt", "w")
+f3 = open("combe_mine_dend.txt", "w")
+f4 = open("combe_mine_sa.txt", "w")
+for sec in cell.secs:
+    if "trunk" in sec.hoc.name():
+        f = f2
+    elif "apic" in sec.hoc.name():
+        f = f1
+    elif "dend" in sec.hoc.name():
+        f = f3
+    else:
+        f = f4
+
+    mechs = sec.hoc.psection()
+    for key in sorted(mechs.keys()):
+        if isinstance(mechs[key], dict):
+            for new_key in  sorted(mechs[key].keys()):
+                f.write("%s %s %s " % (sec.hoc.name(), key, new_key) + str(mechs[key][new_key]) + "\n")
+        else:
+            f.write("%s %s " % (sec.hoc.name(), key) + str(mechs[key]) + "\n")
+
+ax.set_xlabel("time (s)")
+ax.set_ylabel("V (mV)")
+ax.legend()
+plt.show()
