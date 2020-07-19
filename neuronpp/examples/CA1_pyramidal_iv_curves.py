616ee01cb8d2c706a0b13b5eee35e59f1db0b70c
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.edu.pl>
2020-07-19 19:15:23 +0200

    Look both at hyperpolarization and depolarization

--- neuronpp/examples/CA1_pyramidal_iv_curves.py
+++ neuronpp/examples/CA1_pyramidal_iv_curves.py
@@ -12,7 +12,7 @@ cell = CA1PyramidalCell(name="cell1")
 soma = cell.filter_secs("soma")
 
 fig, ax = plt.subplots(1, 1)
-injections = [-.8, -.6, -.2, 0]# .200, .400]
+injections = [-.8, -.6, -.2, 0, .200, .400]
 neuron.h.celsius = 34
 
 for inj in injections:
