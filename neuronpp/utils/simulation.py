e00a64e2aec0e6213dbef8a3636c5195fc57cb7f
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.edu.pl>
2020-10-19 14:25:54 +0200

    Fix broken tests

--- neuronpp/utils/simulation.py
+++ neuronpp/utils/simulation.py
@@ -14,13 +14,12 @@ from neuron.units import ms, mV
 
 h.load_file('stdrun.hoc')
 
+
 class Simulation(NeuronRemovable):
-    def __init__(self, init_v: float = None, dt: float = 0.025,
-                 warmup: float = 0, warmup_dt=None,
-                 init_sleep: float = 0,
-                 shape_plots: Optional[List[h.PlotShape]] = None,
+    def __init__(self, init_v: float = None, dt: float = 0.025, warmup: float = 0, warmup_dt=None,
+                 init_sleep: float = 0, shape_plots: Optional[List[h.PlotShape]] = None,
                  constant_timestep: bool = True, with_neuron_gui: bool = False,
-                 check_pointers: bool = False):
+                 check_pointers: bool = False, warmup_on_create=False):
         """
         Create an Simulation object to control the NEURON's simulation.
 
